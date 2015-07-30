fef800e34c04b138b0d64137638ba2727d8fce20
khanhauphu <ngohuynhngockhanh@gmail.com>
2015-07-30 15:20:55 +0000

    send file via socket

--- svg2gcode.js
+++ svg2gcode.js
@@ -1,12 +1,10 @@
 #!/usr/bin/env node
 //require
 var	express		=	require('express'),
+	siofu 		= 	require("socketio-file-upload")
 	app        	= 	express(),
 	fs         	= 	require('fs'),
 	server		=	require('http').createServer(app),
-	bodyParser	=	require('body-parser'),
-	multipart  	= 	require('connect-multiparty'),
-	multipartMiddleware = multipart(),
     io			=	require('socket.io').listen(server),
 	argv		=	require('optimist').argv,
 	phpjs		= 	require('phpjs'),
@@ -21,10 +19,10 @@ var	express		=	require('express'),
 				});
 
 var	gcodeQueue	= 	[],
-	maxQueue	=	3,				// Tối đa 8 lệnh trong hàng đợi
 	timer1		=	phpjs.time(),
-	okWaitTime	=	10000,				//10s
-	intervalTime	=	argv.waitTime || 1000;	//1s
+	machineRunning=	false,
+	okWaitTime	=	10,				//10s
+	intervalTime	=	argv.waitTime || 1500;	//1s
 
 /*
 
@@ -48,9 +46,6 @@ process.stdin.resume();
 
 */
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
 
 // Thư mục chứa ảnh upload trên server
 app.use('/pictures', express.static(__dirname + '/upload'));
@@ -59,44 +54,30 @@ app.get('/', function(req, res) {
     res.sendfile(__dirname + '/index.html');
 });
 
-// Lắng nghe khi có requrest POST
-app.post('/upload', multipartMiddleware, function(req, res, next) {
-
-    var file = req.files.file;
-
-    // Tên file
-    var originalFilename = file.name;
-
-    // File type
-    var fileType         = file.type.split('/')[1];
-
-    // File size
-    var fileSize         = file.size;
-
-    // Đường dẫn lưu ảnh
-    var pathUpload       = __dirname + '/upload/' + originalFilename;
-
-    // Đọc nội dung file tmp
-    // nếu không có lỗi thì ghi file vào ổ cứng
-    fs.readFile(file.path, function(err, data) {
-        if(!err) {
-        	var svg = svg2gcode.svg2gcode(data.toString(), argv);
-        	fs.writeFile(pathUpload + '.sd', svg, function() {
-        		res.send(phpjs.str_replace("\r", "<br />", svg));
-        		gcodeQueue = phpjs.explode("\r", svg);				      		
-        		return;
-        	});
-        }
-    });
-});
 
 io.sockets.on('connection', function (socket) {
+	var uploader = new siofu();
+    uploader.dir = "./upload";
+    uploader.listen(socket);
+	 // Do something when a file is saved:
+    uploader.on("saved", function(event){
+        var file = event.file;
+		var filepath = file.pathName;
+		var content = fs.readFileSync('./' + filepath).toString();
+		content = svg2gcode.svg2gcode(content, argv);
+		addQueue(content);
+    });
+	// Error handler:
+    uploader.on("error", function(event){
+        console.log("Error from uploader", event);
+    });
 	socket.on('message',function(data){
   		console.log();
 	});
 });
 
 server.listen(90);
+siofu.listen(server);
 console.log('Server runing port 90');
 
 function sendFirstGCodeLine() {
@@ -104,36 +85,61 @@ function sendFirstGCodeLine() {
 		return false;
 	var command = gcodeQueue.shift();
 	serialPort.write(command + "\r");
-	console.log(command);
+	io.sockets.emit("GCODE", {command: command, length: gcodeQueue.length});
+	if (gcodeQueue.length == 0)
+		machineRunning = false;
 	return true;
 }
 
 function sendGcodeFromQueue() {
-
-		sendFirstGCodeLine();
-
+	sendFirstGCodeLine();
 }
 
 function receiveData(data) {
 	if (data.indexOf('<') == 0) {	//type <status,...>
 		data = phpjs.str_replace(['<', '>', 'WPos', 'MPos', ':', "\r", "\n"], '', data);
 		var data_array = phpjs.explode(',', data);
-
-		var status = data_array[0];
+		
+		io.sockets.emit('position', data_array);
 		
 	} else if (data.indexOf('ok') == 0) {
+		timer1 = phpjs.time();
 		sendGcodeFromQueue();
-
 	}
+		
 }
 
-serialPort.on("open", function () {
-	console.log('open serial port');
-	serialPort.on('data', function(data) {
-		 receiveData(data);
-	});
+function addQueue(list) {
+	if (phpjs.is_string(list)) {
+		if (list.indexOf("\r") > -1) {
+			list = phpjs.str_replace("\n", '', list);
+			list = phpjs.explode("\r", list);
+		} else
+			list = phpjs.explode("\n", list);
+	}
+	gcodeQueue = list;
+}
+
+serialPort.on("open", function (error) {
+	if (error) {
+		console.log(error);
+		io.sockets.emit("error", {id: 0, message: "Can't open Serial port", error: error});
+	} else {
+		console.log('open serial port');
+		var interval = setInterval(function() {
+			serialPort.write("?\r", function (e) {
+				if (e != undefined)
+					io.sockets.emit('error');
+			});
+		}, intervalTime);
+		serialPort.on('data', function(data) {
+			 receiveData(data);
+		});
+	}
 });
 
 var AT_interval = setInterval(function() {
 	serialPort.write("?\r");
+	if (machineRunning && phpjs.time() - timer1 > okWaitTime) 
+		io.socket.emit("error", {id: 0, message: 'Long time to wait ok response'});
 }, intervalTime);
